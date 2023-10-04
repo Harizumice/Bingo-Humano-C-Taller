9f1bc9070794a4469335369ed3bc8bba3209aea7
Juan D.Galvis <108950779+Harizumice@users.noreply.github.com>
2023-10-04 16:01:03 -0500

    Finish Project

---
+++ GalvisBingoHumano/Program.cs
@@ -0,0 +1,137 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// Sabe Nadar
+string respuesta;
+
+Console.WriteLine("¿La persona sabe nadar?");
+respuesta = Console.ReadLine();
+
+Console.WriteLine("En el campo, sabe nadar, firmó: " + respuesta);
+Console.WriteLine("-------");
+
+// Le gustan los buñuelos
+Console.WriteLine("¿La persona le gustan los buñuelos?");
+respuesta = Console.ReadLine();
+
+Console.WriteLine("En el campo, en donde le gustan los buñuelos, firmó: " + respuesta);
+Console.WriteLine("-------");
+
+// Le Gusta Bailar
+Console.WriteLine("¿La persona le gusta bailar?");
+respuesta = Console.ReadLine();
+
+Console.WriteLine("En el campo, donde le gusta bailar, firmó: " + respuesta);
+Console.WriteLine("-------");
+
+// Le Recibí un Abrazo
+Console.WriteLine("¿Le recibí un abrazo a la persona?");
+respuesta = Console.ReadLine();
+
+Console.WriteLine("En el campo, donde le recibí un abrazo a la persona, firmó: " + respuesta);
+Console.WriteLine("-------");
+
+// Tenemos el mismo color favorito
+Console.WriteLine("¿La persona tiene mi mismo color favorito?");
+respuesta = Console.ReadLine();
+
+Console.WriteLine("En el campo, la persona tiene el mismo color favorito mío, firmó: " + respuesta);
+Console.WriteLine("-------");
+
+// El mismo Pasatiempo.
+Console.WriteLine("¿La persona tiene el mismo pasatiempo mío?");
+respuesta = Console.ReadLine();
+
+Console.WriteLine("En el campo, donde la persona tiene el mismo pasatiempo favorito, firmó: " + respuesta);
+Console.WriteLine("-------");
+
+// Es hijo único.
+Console.WriteLine("¿La persona es hija única?");
+respuesta = Console.ReadLine();
+
+Console.WriteLine("En el campo, la persona es hij@ único, firmó: " + respuesta);
+Console.WriteLine("-------");
+
+// Le gusta investigar?
+Console.WriteLine("¿La persona le gusta investigar?");
+respuesta = Console.ReadLine();
+
+Console.WriteLine("En el campo, la persona le gusta investigar, firmó: " + respuesta);
+Console.WriteLine("-------");
+
+// No le gusta el futboll
+Console.WriteLine("¿La persona no le gusta el futboll?");
+respuesta = Console.ReadLine();
+
+Console.WriteLine("En el campo, donde la persona no le gusta el futboll, firmó: " + respuesta);
+Console.WriteLine("-------");
+
+// Le agradan los niños.
+Console.WriteLine("¿A la persona le agradan los niños?");
+respuesta = Console.ReadLine();
+
+Console.WriteLine("En el campo, la persona le agradan los niños, firmó: " + respuesta);
+Console.WriteLine("-------");
+
+// Le gusta la natilla
+Console.WriteLine("¿A la persona le gusta la natilla?");
+respuesta = Console.ReadLine();
+
+Console.WriteLine("En el campo, donde la persona le gusta la natilla, firmó: " + respuesta);
+Console.WriteLine("-------");
+
+// No es de su mismo género.
+Console.WriteLine("¿La persona no es de su mismo género?");
+respuesta = Console.ReadLine();
+
+Console.WriteLine("En el campo, la persona no es de su mismo género, firmó: " + respuesta);
+Console.WriteLine("-------");
+
+// Le gusta hacer deporte
+Console.WriteLine("¿La persona le gusta hacer deporte?");
+respuesta = Console.ReadLine();
+
+Console.WriteLine("En el campo, la persona le gustan los deportes, firmó: " + respuesta);
+Console.WriteLine("-------");
+
+// Le gustan las matemáticas?
+Console.WriteLine("¿La persona le gustan las matemáticas?");
+respuesta = Console.ReadLine();
+
+Console.WriteLine("En el campo, donde la persona le gustan las Matemáticas, firmó: " + respuesta);
+Console.WriteLine("-------");
+
+// Le gusta leer?
+Console.WriteLine("¿La persona le gusta leer?");
+respuesta = Console.ReadLine();
+
+Console.WriteLine("En el campo, la persona le gusta leer, firmó: " + respuesta);
+Console.WriteLine("-------");
+
+// Se ha ganado algo
+Console.WriteLine("¿La persona se ha ganado algo?");
+respuesta = Console.ReadLine();
+
+Console.WriteLine("En el campo, donde la persona se ha ganado algo, firmó: " + respuesta);
+Console.WriteLine("-------");
+
+// Nacio el Mismo Mes
+Console.WriteLine("¿La persona nacio el mismo mes suyo?");
+respuesta = Console.ReadLine();
+
+Console.WriteLine("En el campo, donde la persona nació el mismo mes suyo, firmó: " + respuesta);
+Console.WriteLine("-------");
+
+// Tiene ojos oscuros.
+Console.WriteLine("¿La persona tiene ojos oscuros?");
+respuesta = Console.ReadLine();
+
+Console.WriteLine("En el campo, donde la persona tiene los ojos oscuros, firmó: " + respuesta);
+Console.WriteLine("-------");
+
+// Su familia vive en otra ciudad
+Console.WriteLine("¿La persona tiene familia en otra ciudad?");
+respuesta = Console.ReadLine();
+
+Console.WriteLine("En el campo, La persona tiene familia en otro lugar, firmó: " + respuesta);
+Console.WriteLine("-------");
